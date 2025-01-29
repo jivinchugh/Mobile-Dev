13dcf9e3d1dfbaee409c60c46493faf62cc9af86
Jivin Chugh <jivinchugh18@gmail.com>
2025-01-29 15:21:29 -0500

    native styled button

--- user-input01/App.js
+++ user-input01/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ // click handler for the button
+ const buttonPressed = () => {
+   console.log("button working")   
+ }
+
+ return (
+   <View style={styles.container}>
+     <Text style={[styles.heading, {marginTop:60}]}>User Input and Output</Text>
+     <Text style={styles.text}>Press the button to see output:</Text>
+     <Button onPress={buttonPressed} title="Click Me!"/>          
+     <Text style={styles.text}></Text>
+   </View>
+ );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   //marginTop:60,   
+ },
+ heading: {
+   fontSize:20, 
+   marginVertical:8, 
+   textAlign:"center",
+ },
+ text: {
+   fontSize:16,
+   marginVertical:8,
+   textAlign:"center"
+ }
 });
