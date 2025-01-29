ff5753a2d60dd8b62b7e103f95ae8e5df42eb0c1
Jivin Chugh <jivinchugh18@gmail.com>
2025-01-28 23:46:24 -0500

    welcome screen with an image asset

--- welcome-screen/App.js
+++ welcome-screen/App.js
@@ -5,11 +5,11 @@ export default function App() {
  return (
    <View style={[styles.container, {justifyContent:"center", alignItems:"center", gap:10}]}>
      <Image
-       source={require("./assets/shape01.png")}
-       style={{width:200, height:200, borderWidth:1}}
+       source={require("./assets/shape04.png")}
+       style={{width:200, height:200}}
      />
-     <Text style={{borderWidth:1, fontSize:40, fontWeight:"bold"}}>Welcome</Text>
-     <Text style={{borderWidth:1, fontSize:20, fontStyle:"italic"}}>We are so glad you are here</Text>
+     <Text style={{fontSize:40, fontWeight:"bold"}}>Welcome</Text>
+     <Text style={{fontSize:20, fontStyle:"italic"}}>We are not so glad that you are here</Text>
    </View>
  );
 }
@@ -19,8 +19,8 @@ const styles = StyleSheet.create({
  container: {
    flex: 1,
    backgroundColor: "#fff200",
-   borderWidth:10,
-   borderColor:"magenta",    
+   borderWidth:1,
+   borderColor:"green",    
    marginTop:60,
  },
  centered: {
