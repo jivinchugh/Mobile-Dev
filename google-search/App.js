981e62559fcf56a76efb826489d8ca6ee4c23301
Jivin Chugh <jivinchugh18@gmail.com>
2025-01-29 00:18:16 -0500

    google searchbar with weburi gif, and community icons

--- google-search/App.js
+++ google-search/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container]}>
+      <View style={[{ flexDirection: "row", justifyContent: "flex-end", gap: 15, borderWidth: 1, borderColor: "red", alignItems: "center" }]}>
+        <Text>Gmail</Text>
+        <Text>Images</Text>
+        <MaterialCommunityIcons name="dots-grid" size={50} color="black" />
+        <View style={{ borderWidth: 1, borderColor:"#BDB0A6", height: 50, width: 50, borderRadius: 25, justifyContent: "center", alignItems: "center", backgroundColor: "#d5c7bc" }}>
+          <Text>JC</Text>
+        </View>
+      </View>
+      <Image
+        source={{ uri: "https://www.google.com/logos/doodles/2023/2023-womens-world-cup-opening-day-6753651837110060-2xa.gif" }}
+        style={{ width: 400, height: 140, borderColor: "blue", borderWidth: 1 }}
+      />
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d8dbe2",
+    borderWidth: 1,
+    borderColor: "green",
+    marginTop: 60,
   },
+  centered: {
+    // textAlign:"center"
+  }
 });
+
+
