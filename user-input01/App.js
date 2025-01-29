f26e2da78ae794f857249338771b0067a52e8d96
Jivin Chugh <jivinchugh18@gmail.com>
2025-01-29 16:10:23 -0500

    useState variable functionality

--- user-input01/App.js
+++ user-input01/App.js
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
-
+import { useState } from 'react';
 
 export default function App() {
+
+  //create state variable
+  //variable needs name
+  //variable needs initial value
+  //const msg - "hello" 
+  const [msg, setMsg] = useState("hello")
+  //msg - name of state variable
+  //setMsg - function to update the state variable
+
+
+
+
   // click handler for the button
   const buttonPressed = () => {
     console.log("native button working")
+
+    const randomvalue = Math.floor(Math.random() * (200-5 + 1) + 5)
+    setMsg("Random value is: " + randomvalue)
   }
+  // click handler for pressable
   const buttonPressed2 = () => {
     alert("Pressable button working!!")
   }
@@ -15,15 +31,15 @@ export default function App() {
       <Text style={[styles.heading, { marginTop: 60 }]}>User Input and Output</Text>
       <Text style={styles.text}>Press the button to see output:</Text>
       <Button onPress={buttonPressed} title="Click Me!" />
+      <Text style={styles.text}>
+        {msg}
+      </Text>
 
       <Text style={styles.text}>Pressable button here</Text>
       <Pressable style={{borderWidth:1, borderColor:"black", paddingVertical:2, paddingHorizontal:1, marginHorizontal:120}} onPress={buttonPressed2}>
         <Text style={[styles.text, {color:"blue"}]}>Pressable Button</Text>
       </Pressable>
-
-
-
-      <Text style={styles.text}></Text>
+      
     </View>
   );
 }
