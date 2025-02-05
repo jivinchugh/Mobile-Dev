375ed1b6273ea8e0e7e4df71bb011b9f481e3adb
Jivin Chugh <jivinchugh18@gmail.com>
2025-02-05 17:34:26 -0500

    added conditional statements

--- conditional-exercise/App.js
+++ conditional-exercise/App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [name, setName] = useState("")
+  const [showText, setShowText] = useState(false)
+  const buttonpressed = () => {
+
+    const Input=parseInt(name)
+    if(Input>20){
+      alert("Input is greater than 20")
+      setShowText(true)
+    }
+    else{
+      alert("Input is less than 20")
+      setShowText(false)
+    }
+
+  };
+
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Conditional!!</Text>
+      <TextInput
+      style={{width: 300, height:40, borderColor: 'gray', borderWidth: 1}}
+      value={name}
+      onChangeText={setName}
+      placeholder='Enter your name'
+      />
+      <Button title='getName' onPress={buttonpressed} />
+
+      {
+        (showText) && 
+        <Text style={{fontSize: 20, color: 'red'}}>This is a text</Text> 
+      }
+
+
     </View>
   );
 }
