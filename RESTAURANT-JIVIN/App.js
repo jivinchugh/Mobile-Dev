a0bb15ed4124dc38bad99e072e157066e7106552
Jivin Chugh <jivinchugh18@gmail.com>
2025-02-23 13:45:49 -0500

    stacking screens to App.js

--- RESTAURANT-JIVIN/App.js
+++ RESTAURANT-JIVIN/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MenuScreen from "./screens/Menu";
+import ReceiptScreen from "./screens/Receipt";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>CHECKKKKKKKKKK!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NANDO's">
+        <Stack.Screen name="NANDO's" component={MenuScreen}
+         options={() => ({          
+          headerTintColor:"black",
+          headerStyle:{backgroundColor: "#ff6b6b"},     
+          })}/>
+        <Stack.Screen name="RECEIPT" component={ReceiptScreen}
+         options={() => ({          
+          headerTintColor:"black",
+          headerStyle:{backgroundColor: "#ff6b6b"},     
+          })}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
