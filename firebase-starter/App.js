aa60100d45729f5c8490cc96c9f7a143c13fb826
Jivin Chugh <jivinchugh18@gmail.com>
2025-04-06 00:44:00 -0400

    added screens

--- firebase-starter/App.js
+++ firebase-starter/App.js
@@ -1,35 +1,37 @@
-import {View, Button} from "react-native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import EntryFormScreen from './screens/EntryFormScreen';
+import EntryFormScreen from "./screens/EntryFormScreen";
 import StudentListScreen from "./screens/StudentListScreen";
-
+import LoginScreen from "./screens/LoginScreen";
+import { Button } from "react-native";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
- 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
-          name="Entry Form" 
-          component={EntryFormScreen}
-          options={ ({ navigation }) => ({
-            headerRight: () => (
-              <Button onPress={
-                () => {
-                  navigation.navigate("Student List Screen")
-                }
-              } 
-              title="All Students"/>
-            )
-          }) }
-        />
+        <Stack.Screen name="Login Screen" component={LoginScreen} />
 
-        <Stack.Screen name="Student List Screen" component={StudentListScreen} />
+        <Stack.Screen
+          name="Entry Form"
+          component={EntryFormScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Button
+                onPress={() => {
+                  navigation.navigate("Student List Screen");
+                }}
+                title="All Students"
+              />
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Student List Screen"
+          component={StudentListScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
