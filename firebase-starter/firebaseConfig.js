4517a999688aab0d80afdc3621ed5fa8a3148ff7
Jivin Chugh <jivinchugh18@gmail.com>
2025-04-05 21:02:21 -0400

    firebase config file

--- firebase-starter/firebaseConfig.js
+++ firebase-starter/firebaseConfig.js
@@ -1,4 +1,26 @@
-// TODO: Add your firebase connection code here
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+//1. import firestore service from firebase/firestore
+import { getFirestore } from "firebase/firestore/lite";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgAKxmP1xNQk4mXLkSJDhkPdS3wdTVF60",
+  authDomain: "test-app-c6fee.firebaseapp.com",
+  projectId: "test-app-c6fee",
+  storageBucket: "test-app-c6fee.firebasestorage.app",
+  messagingSenderId: "286759387223",
+  appId: "1:286759387223:web:96e354304e5e959b4b2312"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-// TODO: Export your required firebase services 
+//2. initialize firestore service
+const db = getFirestore(app);
+//3. export firestore service for use in other files
+export { db };
