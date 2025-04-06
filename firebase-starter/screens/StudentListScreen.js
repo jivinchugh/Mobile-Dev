5d4a992e0786653bdf51548f3adf018978909369
Jivin Chugh <jivinchugh18@gmail.com>
2025-04-06 00:45:36 -0400

    update student logic

--- firebase-starter/screens/StudentListScreen.js
+++ firebase-starter/screens/StudentListScreen.js
@@ -1,110 +1,175 @@
-import { StyleSheet, Text, View, Pressable, TextInput, FlatList} from 'react-native';
-import { useState } from "react"
-import EntryFormScreen from './EntryFormScreen';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TextInput,
+  FlatList,
+} from "react-native";
+import { useState, useEffect } from "react";
 
 // TODO: import the required service from FirebaseConfig.js
+import { db } from "../firebaseConfig";
 
 // TODO: import the specific functions from the service
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
+// icon
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default StudentListScreen = () => {
+  // state variable for the text box
+  const [nameFromUI, setNameFromUI] = useState("");
 
-    // state variable for the text box
-    const [nameFromUI, setNameFromUI] = useState("")
+  // state variable to store students
+  const [studentList, setStudentList] = useState([]);
 
-    // state variable to store students
-    const [studentList, setStudentList] = useState([
-        {name:"Peter Smith", gpa:3.0, tuitionPaid:true, id:"psmith"},
-        {name:"Emily Patel", gpa:4.0, tuitionPaid:true, id:"epatel"},
-        {name:"Suzy Lee", gpa:2.5, tuitionPaid:false, id:"slee"},
-    ])
-       
+  // 1. retrieve data from database
+  const getData = async (studentName) => {
+    try {
+      // 2. after retrieving data, save data to a state variable
+      const tempArray = [];
 
-    // button click handler
-    const btnGetStudentsPressed = async () => {
-        alert(`Textbox value is: ${nameFromUI}`)
+      // gets all documents from the "students"
+      // const querySnapshot = await getDocs( collection(db, "students") )
 
-        // 1. retrieve data from database
-        try {           
+      // get all documents where the gpa >= 2.5
+      //const q = query(collection(db, "students"), where("gpa", ">=", 88));
+      // const q = query(collection(db, "students"), where("isPostGrad", "==", true));
 
-            // 2. after retrieving data, save data to a state variable
+      let querySnapshot = undefined;
+      if (studentName === undefined) {
+        // if no value is provided to the parameter, then it will be undefined
+        querySnapshot = await getDocs(collection(db, "students"));
+      } else {
+        const q = query(
+          collection(db, "students"),
+          where("name", "==", studentName)
+        );
+        querySnapshot = await getDocs(q);
+      }
 
-            // 3. when the state variable updates, the list will auto update
-        } catch (err) {
-            console.log(err)
-        }
-        
+      querySnapshot.forEach((currDoc) => {
+        console.log(`Document id: ${currDoc.id}`);
+        console.log("Document data:");
+        console.log(currDoc.data());
+
+        // you cannot use the data as is to populate the flatlist
+        // create a brand new javascript object that contains the info in the document
+
+        const tempObject = { ...currDoc.data(), id: currDoc.id };
+        tempArray.push(tempObject);
+      });
+
+      setStudentList([...tempArray]);
+
+      // 3. when the state variable updates, the list will auto update
+    } catch (err) {
+      console.log(err);
     }
-  
+  };
 
+  // button click handler
+  const btnGetStudentsPressed = async () => {
+    alert(`Textbox value is: ${nameFromUI}`);
+    getData(nameFromUI);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
-   return(
-       <View style={styles.container}>  
-          <TextInput placeholder="Enter name" onChangeText={setNameFromUI} text={nameFromUI} style={styles.tb}/>
-          <Pressable style={styles.btn} onPress={btnGetStudentsPressed}>
-               <Text style={styles.btnLabel}>Get from Database</Text>
-          </Pressable>
-          
-          <Text style={styles.text}>Class List</Text>
-          <FlatList
-            data={studentList}
-            keyExtractor={(item)=>{ return item.id }}
-            renderItem={
-                    ({item})=>{
-                        return(
-                            <View>
-                                <Text>Name: {item.name}</Text>
-                                <Text>GPA: {item.gpa}</Text>
-                            </View>
-                        )
-                    }
-                }  
-            ItemSeparatorComponent={
-                ()=>{
-                  return(
-                    <View style={{borderWidth:1, borderColor:"#ccc", marginVertical:4}}></View>
-                  )
-                }
-              }
+  const updatePressed = async (item) => {
+    // JSON.stinrigy wil fix the [object object] output
+    alert(item.id);
+    // update the corresponding in the db
+    await updateDoc(doc(db, "students", item.id), { gpa: -25.555 });
 
-            />
-      </View>
+    // refresh the user interface
+    getData();
+  };
 
-   )
-}
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Enter name"
+        onChangeText={setNameFromUI}
+        text={nameFromUI}
+        style={styles.tb}
+      />
+      <Pressable style={styles.btn} onPress={btnGetStudentsPressed}>
+        <Text style={styles.btnLabel}>Get from Database</Text>
+      </Pressable>
 
+      <Text style={styles.text}>Class List</Text>
+      <FlatList
+        data={studentList}
+        keyExtractor={(item) => {
+          return item.id;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>Name: {item.name}</Text>
+              <Text>GPA: {item.gpa}</Text>
+              <Pressable
+                onPress={() => {
+                  updatePressed(item);
+                }}
+              >
+                <FontAwesome5 name="edit" size={24} color="black" />
+              </Pressable>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{ borderWidth: 1, borderColor: "#ccc", marginVertical: 4 }}
+            ></View>
+          );
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',     
-     padding:20,
-   },
-   tb: {
-       width:"100%",   
-       borderRadius:5,
-       backgroundColor:"#efefef",
-       color:"#333",
-       fontWeight:"bold", 
-       paddingHorizontal:10,
-       paddingVertical:15,
-       marginVertical:10,       
-   },
-   btn: {
-       borderWidth:1,
-       borderColor:"#141D21",
-       borderRadius:8,
-       paddingVertical:16,
-       marginVertical:20
-   },
-   btnLabel: {
-       fontSize:16,
-       textAlign:"center"
-   },
-   text: {
-    fontSize:20,
-    textAlign:"center",
-    marginVertical:8,
-   }
-   
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  tb: {
+    width: "100%",
+    borderRadius: 5,
+    backgroundColor: "#efefef",
+    color: "#333",
+    fontWeight: "bold",
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    marginVertical: 10,
+  },
+  btn: {
+    borderWidth: 1,
+    borderColor: "#141D21",
+    borderRadius: 8,
+    paddingVertical: 16,
+    marginVertical: 20,
+  },
+  btnLabel: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 8,
+  },
 });
